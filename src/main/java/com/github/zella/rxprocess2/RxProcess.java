267e8aa7abbf38f7e7d00f825fea54805541d9a5
zella <drumirage@gmail.com>
2019-08-20 21:55:25 +0300

    add non blocking impl

--- src/main/java/com/github/zella/rxprocess2/RxProcess.java
+++ src/main/java/com/github/zella/rxprocess2/RxProcess.java
@@ -1,6 +1,7 @@
 package com.github.zella.rxprocess2;
 
 import com.github.zella.rxprocess2.impl.javalang.JavaReactiveProcessBuilder;
+import com.github.zella.rxprocess2.impl.nuprocess.NuNonBlockingReactiveProcessBuilder;
 import com.github.zella.rxprocess2.impl.nuprocess.NuReactiveProcessBuilder;
 import com.zaxxer.nuprocess.NuProcess;
 import com.zaxxer.nuprocess.NuProcessBuilder;
@@ -12,23 +13,38 @@ public final class RxProcess {
     }
 
     /**
-     * Create reactive api for process builder
+     * Create blocking reactive api for process builder.
+     * <p>
+     * Note, that it blocks thread until process ends. If you want run few processes at constant parallelism level, you prefer blocking implementations.
      *
      * @param builder
-     * @return Reactive representation of process puilder
+     * @return Reactive representation of process builder
      */
     public static IReactiveProcessBuilder<Process> reactive(ProcessBuilder builder) {
         return new JavaReactiveProcessBuilder(builder);
     }
 
     /**
-     * Create reactive api for process builder
+     * Create blocking reactive api for process builder
+     * <p>
+     * Note, that it blocks thread until process ends. If you want run few processes at constant parallelism level, you prefer blocking implementations.
      *
      * @param builder
-     * @return Reactive representation of process puilder
+     * @return Reactive representation of process builder
      */
     public static IReactiveProcessBuilder<NuProcess> reactive(NuProcessBuilder builder) {
         return new NuReactiveProcessBuilder(builder);
     }
+
+    /**
+     * Create reactive non blocking api for process builder
+     *
+     * @param builder
+     * @return Non blocking reactive representation of process builder
+     */
+    public static IReactiveProcessBuilder<NuProcess> reactiveNonBlocking(NuProcessBuilder builder) {
+        return new NuNonBlockingReactiveProcessBuilder(builder);
+    }
+
 
 }
---
+++ src/main/java/com/github/zella/rxprocess2/impl/nuprocess/NuNonBlockingReactiveProcess.java
@@ -0,0 +1,104 @@
+package com.github.zella.rxprocess2.impl.nuprocess;
+
+import com.github.zella.rxprocess2.BaseReactiveProcess;
+import com.github.zella.rxprocess2.Exit;
+import com.github.zella.rxprocess2.ProcessChunk;
+import com.github.zella.rxprocess2.common.ArrayUtils;
+import com.github.zella.rxprocess2.errors.ProcessException;
+import com.github.zella.rxprocess2.errors.ProcessTimeoutException;
+import com.zaxxer.nuprocess.NuProcess;
+import com.zaxxer.nuprocess.NuProcessBuilder;
+import io.reactivex.Single;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static com.github.zella.rxprocess2.RxProcessConfig.GRACEFULL_STOP_SECONDS;
+
+public class NuNonBlockingReactiveProcess extends BaseReactiveProcess<NuProcess> {
+
+    private final NuProcessBuilder builder;
+
+    NuNonBlockingReactiveProcess(NuProcessBuilder builder) {
+        this.builder = builder;
+    }
+
+    @Override
+    public Single<Exit> waitDone(long timeout, TimeUnit timeUnit) {
+
+        return Single.<Exit>create(emitter -> {
+
+            builder.setProcessListener(new BaseNuProcessHandler() {
+                @Override
+                void onNext(ProcessChunk chunk) {
+                    stdoutStdErrSubject.onNext(chunk);
+
+                    if (chunk.isStdErr) {
+                        synchronized (emitter) {
+                            for (byte b1 : chunk.data) {
+                                stderrBuffer.add(b1);
+                            }
+                        }
+                    }
+                }
+
+                @Override
+                void onComplete(int exitValue) {
+                    stdoutStdErrSubject.onComplete();
+                    if (!emitter.isDisposed()) {
+                        if (exitValue != 0) {
+                            synchronized (emitter) {
+                                String err = new String(ArrayUtils.toPrimitive(stderrBuffer.toArray(new Byte[0])));
+                                emitter.onSuccess(new Exit(exitValue, new ProcessException(exitValue, err)));
+                            }
+                        } else {
+                            emitter.onSuccess(new Exit(0));
+                        }
+                    }
+                }
+
+                @Override
+                void started(NuProcess nuProcess) {
+                    stdinProcessor.subscribe(
+                            bytes -> {
+                                synchronized (emitter) {
+                                    nuProcess.writeStdin(ByteBuffer.wrap(bytes));
+                                }
+                            },
+                            err -> nuProcess.destroy(true),
+                            () -> {
+                                synchronized (emitter) {
+                                    nuProcess.closeStdin(false);
+                                }
+                            });
+                }
+
+                @Override
+                void stdoutClosed() {
+
+                }
+
+                @Override
+                void stderrClosed() {
+
+                }
+            });
+
+            NuProcess process = builder.start();
+
+            startedSubject.onNext(process);
+            startedSubject.onComplete();
+            emitter.setCancellable(() -> process.destroy(true));
+        }).compose(s -> {
+            //TODO revision
+            if (timeout == -1) return s;
+            else
+                return s.timeout(timeout, timeUnit).onErrorReturn(e -> new Exit(Integer.MIN_VALUE, (e instanceof TimeoutException)
+                        ? new ProcessTimeoutException(Integer.MIN_VALUE)
+                        : new ProcessException(Integer.MIN_VALUE, e.getMessage()))
+                );
+        });
+    }
+
+}
